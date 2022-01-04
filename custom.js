6db5ab18fbecc60a3a05e2e52578fc400bf1b153
Muhammad-Hamza-Tahir <hamzatahir8935@gmail.com>
2022-01-04 19:52:48 +0500

    Create custom.js

---
+++ custom.js
@@ -0,0 +1,34 @@
+let input = document.getElementById('input');
+let input1 = document.getElementById('input1');
+let result = document.getElementById('result');
+let green = document.querySelector('.green');
+let red = document.querySelector('.red');
+let article1 = document.querySelector('.article');
+let btn = document.getElementById('btn');
+let text = document.getElementById('text');
+let rupees = document.getElementById('rupees');
+let article = document.getElementById('article');
+
+
+
+   btn.addEventListener('click',function () {
+     let  input11 = input.value;
+     let input12 = input1.value;
+     
+     if (input12 > 0 ) {
+         green.innerText = input12;
+     } else{
+         red.innerText = input12;
+     }
+ 
+       let element = document.createElement('article')
+       element.innerHTML = ` <div id="article">
+    
+       <div id="text">${input11}</div>
+       <div id="rupees">${input12}</div> 
+       </div> `
+       console.log(element);
+
+       article.appendChild(element);
+
+   })
