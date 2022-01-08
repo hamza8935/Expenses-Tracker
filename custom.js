0225d664fa9c4ca01f5e403d96990d1bdea98626
Muhammad-Hamza-Tahir <hamzatahir8935@gmail.com>
2022-01-08 19:48:56 +0500

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,3 +1,4 @@
+let arr = [] ;
 let input = document.getElementById('input');
 let input1 = document.getElementById('input1');
 let result = document.getElementById('result');
@@ -11,15 +12,25 @@ let item = document.getElementById('main-item');
 
    btn.addEventListener('click',function (event) {
      event.preventDefault()
-     let  input11 = input.value;
+    
      
      let input12 = input1.value
      if (input12 > 0 ) {
+         //result.innerText = input12;
          green.innerText =`$ ${input12}`;
      } else{
+        // result.innerText = input12;
          red.innerText =`$ ${input12}`;
      }
- 
+     let  input11 = input.value;
+     
+
+      arr.push(input12)
+     console.log(arr);  
+  let arr1 = arr.reduce(function (prev , curr){
+       return prev + curr ;
+     })
+     result.innerText = arr1 ;
        let element = document.createElement('div')
        element.setAttribute('id','article')
        element.innerHTML = ` 
@@ -33,7 +44,7 @@ let item = document.getElementById('main-item');
        
        console.log(element);
        item.appendChild(element);
-       result.innerText = input12;
+      //  result.innerText = input12;
 
 // let arr = [];
 // arr.unshift(input12);
